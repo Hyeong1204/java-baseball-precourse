c2cd8b1aa00b5cfc43591fede49f11eea120bb9c
Hyeong1204 <cho4870@naver.com>
2023-07-07 22:48:01 +0900

    feat: Baseball Test 추가

---
+++ src/test/java/baseball/Baseball.java
@@ -0,0 +1,16 @@
+package baseball;
+
+import org.junit.jupiter.api.Test;
+
+public class Baseball {
+    
+    @Test
+    public void printTest() {
+        System.out.println("야구 게임 테스트");
+    }
+
+    @Test
+    public void printBaseball() {
+        System.out.println("야구 테스트");
+    }
+}
